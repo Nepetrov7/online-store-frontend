5e38c14dd9962f566c2d9c52507270841df37b47
Vladislav Nozhkin <VNozhkin@exolve.ru>
2025-05-12 16:15:47 +0300

    added Loader

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from '@/app/router/routes';
 import { Header } from '@/widgets/Header';
 import { Container } from '@/shared/components/Container/Container';
+import { Loader } from '@/shared/ui/Loader/Loader';
 
 export const App = () => {
     return (
         <BrowserRouter>
             <Container>
                 <Header />
-                <React.Suspense fallback={<div>Loading...</div>}>
+                <React.Suspense fallback={<Loader />}>
                     <AppRoutes />
                 </React.Suspense>
             </Container>
