b9c84d62bf39fb1d549605749e4559b90938dd01
Vladislav Nozhkin <VNozhkin@exolve.ru>
2025-05-12 17:04:18 +0300

    update NotFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './NotFound.module.css';
+import { Container } from '@/shared/components/Container';
 
 export const NotFound = () => (
     <div className={styles.container}>
         <h2>404 - Страница не найдена</h2>
         <p>Такой страницы не существует.</p>
-        <Link to="/">Вернуться на главную</Link>
+        <Link style={{ color: 'var(--color-dark)', textDecoration: 'underline' }} to="/">
+            Вернуться на главную
+        </Link>
     </div>
 );
