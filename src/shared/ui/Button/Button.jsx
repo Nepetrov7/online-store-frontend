c1b378c61ed5efe210b180ec4e3cce28a11cbe15
Vladislav Nozhkin <VNozhkin@exolve.ru>
2025-05-24 00:52:25 +0300

    added new ui components

--- src/shared/ui/Button/Button.jsx
+++ src/shared/ui/Button/Button.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import styles from './Button.module.css';
 
-export const Button = ({ children, onClick }) => (
-    <button className={styles.btn} onClick={onClick}>
+export const Button = ({ children, ...props }) => (
+    <button className={styles.button} {...props}>
         {children}
     </button>
 );
